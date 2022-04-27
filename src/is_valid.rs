38260120bf7a16f1094aafe6e6cabede7949433c
senpai-10 <bmjfdrh@gmail.com>
2022-04-27 10:54:24 +0300

    impl is_valid

--- src/is_valid.rs
+++ src/is_valid.rs
@@ -1,6 +1,14 @@
-// Check if currency is valid
-// use Vector of String's fulled with valid currencies (uppercase)
+use crate::verbose_print;
+use serde_json::Value as JsonValue;
 
-pub fn is_valid(currency: &String) -> bool {
-    return true;
+pub async fn is_valid(currency: &String, verbose: bool) -> Result<bool, reqwest::Error> {
+    let url = "https://api.coinbase.com/v2/currencies";
+
+    verbose_print(format!("GET request to {}", url), verbose);
+
+    let res_text = reqwest::get(url).await?.text().await.unwrap();
+    let parsed_json: JsonValue = res_text.parse().unwrap();
+    let data = &parsed_json["data"].as_array().unwrap();
+
+    Ok(data.iter().any(|i| i["id"] == currency.to_uppercase()))
 }
