3a6d0d8e75589f5716a136c3af276b1feea79a31
senpai-10 <bmjfdrh@gmail.com>
2022-04-27 06:41:24 +0300

    setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+
+use colored::Colorize;
+use reqwest;
+use serde_json::Value as JsonValue;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let res_text = reqwest::get("https://api.coinbase.com/v2/exchange-rates?currency=USD")
+        .await?
+        .text()
+        .await
+        .unwrap();
+
+    let parsed_json: JsonValue = res_text.parse().unwrap();
+    let price = parsed_json["data"]["rates"]["SAR"].as_str().unwrap();
+
+    println!("Price: {}", price);
+
+    Ok(())
 }
