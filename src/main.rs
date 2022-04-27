1fa789a6205d3f37084b4b680fcd11b65067cb31
senpai-10 <bmjfdrh@gmail.com>
2022-04-27 09:15:32 +0300

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 extern crate serde;
 
+mod cli;
+mod help;
+mod is_valid;
+
+use cli::Cli;
 use colored::Colorize;
+use help::help;
+use is_valid::is_valid;
 use reqwest;
 use serde_json::Value as JsonValue;
 
@@ -8,25 +15,50 @@ const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    let res_text = reqwest::get("https://api.coinbase.com/v2/exchange-rates?currency=USD")
-        .await?
-        .text()
-        .await
-        .unwrap();
+    let cli = Cli::new();
 
+    if cli.help {
+        help();
+        std::process::exit(0);
+    } else if cli.version {
+        println!("Version: {}", VERSION.bright_yellow());
+        std::process::exit(0);
+    }
+
+    if !cli.from.is_empty() && cli.to.is_empty() || cli.from.is_empty() && cli.to.is_empty() {
+        println!("You need to provide move info\nExample: 'forx-rs usd aud'\nSee --help");
+        std::process::exit(1);
+    }
+
+    if !is_valid(&cli.from) {
+        println!("'{}' is not a valid currency!", cli.from);
+        std::process::exit(1);
+    }
+    if !is_valid(&cli.to) {
+        println!("'{}' is not a valid currency!", cli.to);
+        std::process::exit(1);
+    }
+
+    let res_text = reqwest::get(format!(
+        "https://api.coinbase.com/v2/exchange-rates?currency={}",
+        cli.from.to_uppercase()
+    ))
+    .await?
+    .text()
+    .await
+    .unwrap();
     let parsed_json: JsonValue = res_text.parse().unwrap();
-    let price_str = match parsed_json["data"]["rates"]["SAR"].as_str() {
+    let price_str = match parsed_json["data"]["rates"][cli.to.to_uppercase()].as_str() {
         Some(price) => price,
         None => {
             println!("Invaild currency!");
             std::process::exit(1);
         }
     };
+    let mut price: f64 = price_str.parse().unwrap();
+    price = price * cli.quantity_value as f64;
 
-    let price: f64 = price_str.parse().unwrap();
-
-    println!("Price: {}", price);
-    println!("Price: {}", price * 2.0);
+    println!("{}", price);
 
     Ok(())
 }
